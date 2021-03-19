d51a064056221a6984c67b7a4f2440f0baa44905
Paulazarte <paulazarte1997@gmail.com>
2021-03-18 23:24:15 -0300

    Comenzando con la página de contacto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div>
       <h1>Hola Mundo</h1>
+      <Contact/>
     </div>
   );
 }
