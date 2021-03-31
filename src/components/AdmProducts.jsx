a5e77794c9f0131b95f30a8458e23c599d176831
Constanza Galía Maldonado <coco.galiam@gmail.com>
2021-03-31 13:19:50 -0300

    Agrego barra lateral en panel admin, rutas anidadas de user y products

---
+++ src/components/AdmProducts.jsx
@@ -0,0 +1,9 @@
+
+
+export default function AdmProducts() {
+  return (
+    <div className="container my-3">
+        <h1>Productos</h1>
+    </div>
+  )
+}
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,8 @@
-import { Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import AdmNavtop from "../components/AdmNavtop";
+import AdmProducts from "../components/AdmProducts";
 import AdmSidebar from "../components/AdmSidebar";
+import AdmUsers from "../components/AdmUsers";
 import '../css/admin.css';
 
 
@@ -8,11 +10,19 @@ export default function Admin() {
   return (
     <>
       <AdmNavtop />
-      <AdmSidebar />
+      <div className="d-flex">
+        <AdmSidebar />
+          <Switch>
+          <Route path='/admin/users'>
+            <AdmUsers />
+          </Route>
+          <Route path='/admin/products'>
+            <AdmProducts />
+          </Route>
+          </Switch>
 
-      <Switch>
-        
-      </Switch>
+      </div>
+
     </>
     
   );
