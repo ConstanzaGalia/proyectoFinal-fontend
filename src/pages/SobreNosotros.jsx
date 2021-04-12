0f8a34589d17a592fd609156afebca3f65285d52
Paulazarte <paulazarte1997@gmail.com>
2021-04-12 01:22:49 -0300

    terminando detalles del front

--- src/pages/SobreNosotros.jsx
+++ src/pages/SobreNosotros.jsx
@@ -26,38 +26,7 @@ export default function SobreNosotros({token, user, setToken}) {
         </div>
 
         <h2 className="text-center mt-5">Nuestros atletas</h2>
-        {/* <div className="d-flex flex-wrap justify-content-center">
-          <div className="mb-4 px-3 imagenNA">
-            <img className="img-fluid"
-              src="https://images.unsplash.com/photo-1533107862482-0e6974b06ec4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=680&q=80"
-              alt=""
-            />
-            <h4 className="text-center mt-3 mb-2">Coco Galía</h4>
-            <p className="text-justify">
-              COCO'S DESCRIPTION Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aperiam perferendis dolore ut. Aperiam, voluptates quidem culpa veniam animi harum!
-            </p>
-          </div>
-          <div className="mb-4 px-3 imagenNA">
-            <img className="img-fluid"
-              src="https://images.unsplash.com/photo-1533107862482-0e6974b06ec4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=680&q=80"
-              alt=""
-            />
-            <h4 className="text-center mt-3 mb-2">Coco Galía</h4>
-            <p className="text-justify">
-              COCO'S DESCRIPTION Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aperiam perferendis dolore ut. Aperiam, voluptates quidem culpa veniam animi harum!
-            </p>
-          </div>
-          <div className="mb-4 px-3 imagenNA">
-            <img className="img-fluid"
-              src="https://images.unsplash.com/photo-1533107862482-0e6974b06ec4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=680&q=80"
-              alt=""
-            />
-            <h4 className="text-center mt-3 mb-2">Juani</h4>
-            <p className="text-justify">
-              COCO'S DESCRIPTION Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aperiam perferendis dolore ut. Aperiam, voluptates quidem culpa veniam animi harum!
-            </p>
-          </div>
-        </div> */}
+       
       </div>
       <SNC></SNC>
     </>
